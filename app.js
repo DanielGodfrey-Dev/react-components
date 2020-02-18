15c31484681e173cf64490c613f4fffe76fe1a5a
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-02-18 11:35:19 -0800

    Add react components for list items
    Adding elements from tutorial and GroceryList exercise.

--- app.js
+++ app.js
@@ -1,1 +1,84 @@
-// TODO
+var GroceryList = (items) => (
+<ul>
+    <li>{items.food[0]}</li>
+    <li>{items.food[1]}</li>
+    <li>{items.food[2]}</li>
+    <li>{items.food[3]}</li>
+</ul>
+);
+
+var Main = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList food={['lettuce', 'avocados', 'steak', 'pile of food']}/> // Here's my GroceryList
+  </div>
+);
+
+ReactDOM.render(<Main/>, document.getElementById('app'));
+
+// Here we create a `TodoList` component
+var TodoList = (props) => {
+
+  // This function will be called when the first `<li>` below is clicked on
+  // Notice that event handling functions receive an `event` object
+  // We want to define it where it has access to `props`
+
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  // Because we used curly braces with this arrow function
+  // we have to write an explicit `return` statement
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.todos[0]}</li>
+      <li>{props.todos[1]}</li>
+      <li>{props.todos[2]}</li>
+    </ul>
+  );
+}
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+  </div>
+);
+
+// A class component can be defined as an ES6 class
+// that extends the base Component class included in the React library
+class TodoListItem extends React.Component {
+
+  // A `constructor` method is expected on all ES6 classes
+  // When React instantiates the component,
+  // it will pass `props` to the constructor
+  constructor(props) {
+    // Equivalent to ES5's React.Component.call(this, props)
+    super(props);
+  }
+
+  // Every class component must have a `render` method
+  // Stateless functional components are pretty much just this method
+  render() {
+
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
+    return (
+      <li>{this.props.todo}</li>
+    );
+
+  }
+
+}
+
+// Update our `TodoList` to use the new `TodoListItem` component
+// This can still be a stateless function component!
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<App/>, document.getElementById('tutorial'));
